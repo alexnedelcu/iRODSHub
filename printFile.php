228e682b19564f0195dec4fab0ce0d4a0971e458
Vu <buidinhvu@gmail.com>
2012-09-05 11:27:07 -0400

    upload file to irods server

--- printFile.php
+++ printFile.php
@@ -4,8 +4,19 @@ $group_id = $_GET["group_id"];
 
 $handle = fopen($group_id.".txt", "w");
 
+$server = $_GET["host"];
+$port = $_GET["port"];
+$acc = $_GET["user"];
+$pwd = $_GET["password"];
+$zone= $_GET["zone"];
+
 //connect the group with a collection on iRods server
-fwrite($handle, "{\"".$path."\" : \"".$group_id."\"}");
+fwrite($handle, "{\"path\" : \"".$path."\",");
+fwrite($handle, "\n\"host\" : "."\"".$server."\",");
+fwrite($handle, "\n\"port\" : "."\"".$port."\",");
+fwrite($handle, "\n\"user\" : "."\"".$acc."\",");
+fwrite($handle, "\n\"password\" : "."\"".$pwd."\",");
+fwrite($handle, "\n\"zone\" : "."\"".$zone."\"}");
 
 fclose($handle);
 ?>
